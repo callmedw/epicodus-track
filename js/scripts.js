048684eec4e4fba49772a24218708e963c97d75d
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-03-24 13:31:40 -0700

    parse the numerical values from the answers to each question

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,17 @@ $(document).ready(function() {
     $("#epicodus-track").show();
     });
 */
-$("#input").submit(function(event) {
+  $("#input").submit(function(event) {
     name = $("input#name").val();
-     $(".name").text(name);
+     $(".name-fill").text(name);
+  });
+
+     $("form#epicodus-track").submit(function(event) {
+       question1 = parseInt($("select#question1").val());
+       question2 = parseInt($("select#question2").val());
+       question3 = parseInt($("select#question3").val());
+       question4 = parseInt($("select#question4").val());
+       question5 = parseInt($("select#question5").val());
 
 
 
