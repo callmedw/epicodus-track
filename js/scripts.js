ff26e2de02a546a17152e9f1c164895e1c552990
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-03-24 11:07:43 -0700

    add submit method to name field and show method to show questions after entering name

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 /*--TOP--*/
 
+  $("#name-start").click(function(){
+    $("#epicodus-track").show();
+    });
 
-
+  $("form#input-group").submit(function(event) {
+    event.preventDefault();
+    var name = $("input#name").val();
+    });
 
 
 
