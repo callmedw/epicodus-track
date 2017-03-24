72ef42d8ef3be5b70d6c4fb1e4c1348fb8be8170
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-03-24 14:19:42 -0700

    add addition to answer values to receive a total

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {
 /*--TOP--*/
 
-/*  $("#name-start").click(function(){
-    $("#epicodus-track").show();
-    });
-*/
+// -- Front End Logic -- //
   $("#input").submit(function(event) {
     name = $("input#name").val();
-     $(".name-fill").text(name);
-  });
+    $(".name-fill").text(name);
+    });
 
-     $("form#epicodus-track").submit(function(event) {
-       question1 = parseInt($("select#question1").val());
-       question2 = parseInt($("select#question2").val());
-       question3 = parseInt($("select#question3").val());
-       question4 = parseInt($("select#question4").val());
-       question5 = parseInt($("select#question5").val());
-
+  $("form#epicodus-track").submit(function(event) {
+    answer1 = parseInt($("select#question1").val());
+    answer2 = parseInt($("select#question2").val());
+    answer3 = parseInt($("select#question3").val());
+    answer4 = parseInt($("select#question4").val());
+    answer5 = parseInt($("select#question5").val());
+    total = add(answer1, answer2, answer3, answer4, answer5);
 
 
     event.preventDefault();
     });
 
 
-
-/*  $("#address-results").show();*/
+// -- Back End Logic -- //
+    add = function(answer1, answer2, answer3, answer4, answer5) {
+    return answer1 + answer2 + answer3 + answer3 + answer4 + answer5;
+  }
 
 /*--BOTTOM--*/
 });
