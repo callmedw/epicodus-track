2e02b349afdd174796465d92d3516757c6f86c22
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-03-24 17:08:22 -0700

    add if then js and background image and font

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-/*--TOP--*/
+/*--XX--*/
 
 // -- Front End Logic -- //
   $("#input").submit(function(event) {
     name = $("input#name").val();
     $(".name-fill").text(name);
+    event.preventDefault();
     });
 
   $("form#epicodus-track").submit(function(event) {
@@ -14,16 +15,25 @@ $(document).ready(function() {
     answer4 = parseInt($("select#question4").val());
     answer5 = parseInt($("select#question5").val());
     total = add(answer1, answer2, answer3, answer4, answer5);
-
-
     event.preventDefault();
-    });
-
+  });
 
 // -- Back End Logic -- //
-    add = function(answer1, answer2, answer3, answer4, answer5) {
+  add = function(answer1, answer2, answer3, answer4, answer5) {
     return answer1 + answer2 + answer3 + answer3 + answer4 + answer5;
   }
 
-/*--BOTTOM--*/
+  if (total === 5) {
+    $("#address-results").show();
+  } else if (total > 5 && < total 14) {
+    $("#address-results1").show();
+  } else if (age=== 14) {
+    $("#address-results2").show();
+  } else {
+    alert("Please make sure all questions have been answered!");
+  }
+
+
+
+/*--XX--*/
 });
