6c382ac21710e767c545d63d6d2d0a7d83420fa2
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-04-02 16:20:23 -0700

    update text color and business logic ifthen

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,43 @@
+
 $(document).ready(function() {
 /*--XX--*/
 
-// -- Front End Logic -- //
+//--front end logic---//
   $("#input").submit(function(event) {
+    event.preventDefault();
     name = $("input#name").val();
     $(".name-fill").text(name);
-    event.preventDefault();
-    });
-
-  $("form#epicodus-track").submit(function(event) {
-    answer1 = parseInt($("select#question1").val());
-    answer2 = parseInt($("select#question2").val());
-    answer3 = parseInt($("select#question3").val());
-    answer4 = parseInt($("select#question4").val());
-    answer5 = parseInt($("select#question5").val());
-    total = add(answer1, answer2, answer3, answer4, answer5);
-    event.preventDefault();
   });
 
-// -- Back End Logic -- //
-  add = function(answer1, answer2, answer3, answer4, answer5) {
-    return answer1 + answer2 + answer3 + answer3 + answer4 + answer5;
+  var add = function(answer1, answer2, answer3, answer4, answer5) {
+  return answer1 + answer2 + answer3 + answer3 + answer4 + answer5
   }
 
-  if (total === 5) {
-    $("#address-results").show();
-  } else if (total > 5 && < total 14) {
-    $("#address-results1").show();
-  } else if (age=== 14) {
-    $("#address-results2").show();
-  } else {
-    alert("Please make sure all questions have been answered!");
-  }
-
+  $("form#epicodus-track").submit(function(event) {
+    event.preventDefault();
+    var answer1 = parseInt($("select#question1").val());
+    var answer2 = parseInt($("select#question2").val());
+    var answer3 = parseInt($("select#question3").val());
+    var answer4 = parseInt($("select#question4").val());
+    var answer5 = parseInt($("select#question5").val());
+    total = add(answer1, answer2, answer3, answer4, answer5);
+    answerIf(total);
+  });
+//--back end logic---//
+  function answerIf(total) {
+    if (total === 5) {
+      $("form#epicodus-track").hide();
+      $("#ruby-results").show();
+    } else if (total > 5 && total < 14) {
+      $("form#epicodus-track").hide();
+      $("#php-results").show();
+    } else if (total === 14) {
+      $("form#epicodus-track").hide();
+      $("#java-results").show();
+    } else {
+      alert("Please make sure all questions have been answered!");
+    }
+  };
 
 
 /*--XX--*/
